abd60431c071e4188634fe3b1313386bc977dc6e
Ayesha-p <64060103+Ayesha-p@users.noreply.github.com>
2021-11-29 23:29:57 -0800

    Add files via upload

---
+++ finallab/finallab/EMPLOYEE.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace finallab
+{
+    abstract public class Employee
+    {
+        int basePay;
+        int hoursWorked;
+        int BASE_HOURS;
+        int overtimePay;
+        int Grosspay;
+        int unitPay;
+        int OVERTIME_RATE;
+        int hourlyPayRate = 200;
+        int overtimeHours;
+        int unitprodused;
+
+        abstract public void CalculateSalary();
+    }
+}
---
+++ finallab/finallab/HOURLYEMPLOYEE.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace finallab
+{
+      public class HourlyEmployee : Employee
+    {
+        int fixedsalary = 500;
+
+      public override  void CalculateSalary()
+        {
+
+            if (hoursWorked > BASE_HOURS)
+            {
+                basePay = hourlyPayRate * BASE_HOURS;
+                overtimePay = hoursWorked - BASE_HOURS;
+                overtimePay = overtimeHours * hourlyPayRate * OVERTIME_RATE;
+                unitPay = 3 * unitprodused;
+                Grosspay = basePay + overtimePay + unitPay;
+            }
+        }
+    }
+}
